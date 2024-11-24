69b95f8f81d686b738810f6d0f5a8286fae60464
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2024-11-24 14:36:53 +0300

    add the footer section

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -102,6 +102,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
